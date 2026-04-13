512eff781d3b18775727daa3d7b8090f120e765e
proggeler <proggeler@de-mes.nl>
2026-04-13 11:32:31 +0200

    configurable dependencies

--- src/AuthenticationServiceProvider.php
+++ src/AuthenticationServiceProvider.php
@@ -10,60 +10,62 @@ use DMT\AuthenticationService\Handlers\UserAuthenticationHandlerInterface;
 use DMT\AuthenticationService\Handlers\TokenAuthenticationHandlerInterface;
 use DMT\AuthenticationService\Mailer\HtmlMailManager;
 use DMT\AuthenticationService\Mailer\MailManagerInterface;
+use DMT\AuthenticationService\Mailer\TextMailManager;
 use DMT\AuthenticationService\Password\NativePasswordHandler;
 use DMT\AuthenticationService\Password\PasswordHandlerInterface;
 use DMT\AuthenticationService\Session\DefaultSessionHandler;
 use DMT\AuthenticationService\Session\SessionHandlerInterface;
-use DMT\DependencyInjection\ConfigurationInterface;
+use DMT\DependencyInjection\Attributes\ConfigValue;
 use DMT\DependencyInjection\Container;
 use DMT\DependencyInjection\Exceptions\NotFoundException;
 use DMT\DependencyInjection\ServiceProviderInterface;
 use DMT\MailService\Adapters\MailAdapterInterface;
-use Twig\Environment;
 
 readonly class AuthenticationServiceProvider implements ServiceProviderInterface
 {
+    public function __construct(
+        #[ConfigValue('authentication.userHandler', EmailPasswordAuthenticationHandler::class)]
+        private string $userEntityClass = EmailPasswordAuthenticationHandler::class,
+        #[ConfigValue('authentication.tokenHandler', TokenAuthenticationHandler::class)]
+        private string $tokenEntityClass = TokenAuthenticationHandler::class,
+        #[ConfigValue('authentication.mailManager', HtmlMailManager::class)]
+        private string $mailManagerClass = TextMailManager::class,
+        #[ConfigValue('authentication.sessionHandler', DefaultSessionHandler::class)]
+        private string $sessionHandler = DefaultSessionHandler::class,
+        #[ConfigValue('authentication.passwordHandler', NativePasswordHandler::class)]
+        private string $passwordHandler = NativePasswordHandler::class,
+    ) {
+    }
+
     public function register(Container $container): void
     {
-        if (!$container->has(Environment::class)) {
-            NotFoundException::throwException(Environment::class);
-        }
-
         if (!$container->has(MailAdapterInterface::class)) {
             NotFoundException::throwException(MailAdapterInterface::class);
         }
 
         $container->set(
             id: MailManagerInterface::class,
-            value: fn (): MailManagerInterface => $container->get(HtmlMailManager::class)
+            value: fn (): MailManagerInterface => $container->get($this->mailManagerClass)
         );
 
         $container->set(
             id: SessionHandlerInterface::class,
-            value: fn (): SessionHandlerInterface => new DefaultSessionHandler()
+            value: fn (): SessionHandlerInterface => $container->get($this->sessionHandler)
         );
 
         $container->set(
             id: PasswordHandlerInterface::class,
-            value: fn (): PasswordHandlerInterface => new NativePasswordHandler()
+            value: fn (): PasswordHandlerInterface => $container->get($this->passwordHandler)
         );
-
-        $config = $container->get(ConfigurationInterface::class);
 
         $container->set(
             id: TokenAuthenticationHandlerInterface::class,
-            value: fn (): TokenAuthenticationHandlerInterface
-                => $container->get(
-                    $config->get('authentication.tokenHandler', TokenAuthenticationHandler::class),
-                )
+            value: fn (): TokenAuthenticationHandlerInterface => $container->get($this->tokenEntityClass)
         );
 
         $container->set(
             id: UserAuthenticationHandlerInterface::class,
-            value: fn (): UserAuthenticationHandlerInterface
-                => $container->get(
-                    $config->get('authentication.userHandler', EmailPasswordAuthenticationHandler::class),
-                )
+            value: fn (): UserAuthenticationHandlerInterface => $container->get($this->userEntityClass)
         );
     }
 }
