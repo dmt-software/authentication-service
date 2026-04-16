b0352f7aadbb2bca83fb5ca4d1341d07e765da7f
proggeler <proggeler@de-mes.nl>
2026-04-16 13:08:26 +0200

    session changes

--- src/Session/DefaultSessionHandler.php
+++ src/Session/DefaultSessionHandler.php
@@ -4,21 +4,31 @@ declare(strict_types=1);
 
 namespace DMT\AuthenticationService\Session;
 
+use DMT\DependencyInjection\Attributes\ConfigValue;
+
 class DefaultSessionHandler implements SessionHandlerInterface
 {
+    public function __construct(
+        #[ConfigValue('authentication.session.secure', true)]
+        bool $secure = false,
+        #[ConfigValue('authentication.session.ttl', 1800)]
+        int $lifetime = 1800
+    ) {
+        session_set_cookie_params([
+            'lifetime' => $lifetime,
+            'httponly' => true,
+            'secure' => $secure,
+            'samesite' => 'Lax',
+        ]);
+
+        ini_set('session.use_strict_mode', '1');
+    }
+
     public function start(): void
     {
         if (session_status() === PHP_SESSION_ACTIVE) {
             return;
         }
-
-        session_set_cookie_params([
-            'httponly' => true,
-            'secure' => true,
-            'samesite' => 'Lax',
-        ]);
-
-        ini_set('session.use_strict_mode', '1');
 
         session_start();
     }
@@ -30,6 +40,8 @@ class DefaultSessionHandler implements SessionHandlerInterface
         session_regenerate_id(true);
 
         $_SESSION[self::USER_ID_KEY] = $userId;
+
+        session_write_close();
     }
 
     public function logout(): void
